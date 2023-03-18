2de4d0add4e63d38bd148f9224a7411096538470
Dan{ } <123217422+DanSSV@users.noreply.github.com>
2023-03-18 14:21:06 +0800

    Add files via upload

--- header.php
+++ header.php
@@ -8,4 +8,12 @@
 <body <?php body_class();?>>
     
 <header class="sticky-top">
+    <div class="container">
+    <?php wp_nav_menu (
+        array(
+            'theme_location' => 'top-menu',
+            'menu_class' => 'navigation'
+        )
+    );?>
+    </div>
 </header>
